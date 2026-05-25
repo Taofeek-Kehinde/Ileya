c35bd6e373b5134af9c43c4144c8962dcfe80759
Taofeek-Kehinde <Ktaofeek015@gmail.com>
2026-05-25 17:35:11 +0100

    updating...

---
+++ dayOne/JavaScript/Level1/TaskTwo.js
@@ -0,0 +1,5 @@
+const list_numbers = [45, 0, 8, 8, 0, 45];
+
+let is_palindrome = list_numbers == list_numbers.reverse();
+
+console.log(is_palindrome);
--- dayOne/Python/Level1/tasktwo.py
+++ dayOne/Python/Level1/tasktwo.py
@@ -1,1 +1,5 @@
+list_numbers = [45, 0, 8, 8, 0, 45]
 
+is_palindrome = list_numbers == list_numbers[::-1]
+
+print(is_palindrome)
